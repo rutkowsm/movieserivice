5cf4c07830cd646695714ae47be74d6dfd73cd06
s12497@pjwstk.edu.pl <Angelo0000>
2022-05-21 13:39:53 +0200

    jpa repo

--- src/main/java/com/movieservice/movieserivice/repository/MovieRepo.java
+++ src/main/java/com/movieservice/movieserivice/repository/MovieRepo.java
@@ -1,9 +1,11 @@
 package com.movieservice.movieserivice.repository;
 
+import com.movieservice.movieserivice.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface MovieRepo extends JpaRepository {
+public interface MovieRepo extends JpaRepository<Movie, Integer> {
 
 }
